99d541c8a4eb59032256dbc1c830efbb1f8a6edf
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-02-08 14:30:50 +0300

    DOM Examination

---
+++ dom.js
@@ -0,0 +1,11 @@
+// EXAMINE THE DOCUMENT OBJECT
+//  console.dir(document);
+//  console.log(document.all[10]);
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// DOM Selectors
